dfcfbbb8c3eb096dbc207041d208474aa93a3d2d
Oleg Ryasnoy <ryasnoy.oleg@gmail.com>
2018-05-15 17:27:42 +0300

    Connect MySQL DataBase

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mysql = require('mysql')
 
 app.use(morgan('short'))
 
@@ -15,6 +16,31 @@ app.listen(3000, () => {
   console.log("Server is up and listening on port 3000");
 })
 
+app.get("/user/:id", (req, res) => {
+    console.log("Fetching user with id: " + req.params.id)
+
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      database: 'lbta_mysql'
+    })
+    const userID =  req.params.id
+    const query = "SELECT * FROM users WHERE id = ?"
+    connection.query(query, [userID], (err, rows, fields) => {
+      if (err) {
+        res.sendStatus(500)
+        return
+        // throw err
+      }
+
+      const users = rows.map((row)=> {
+        return {firstName: row.first_name}
+      })
+
+      res.json(users)
+    })
+})
+
 app.get("/users", (req, res) => {
   const user = {
     first_name: "Steve",
@@ -23,5 +49,5 @@ app.get("/users", (req, res) => {
 
   res.json(user)
 
-  res.send("Nodemon auto updates when I save this file")
 })
+
