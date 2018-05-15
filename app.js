cdebdf666658ef246ce29de39eb380296ad42e0d
Oleg Ryasnoy <ryasnoy.oleg@gmail.com>
2018-05-15 18:31:26 +0300

    POST user_create *from HTML form

--- app.js
+++ app.js
@@ -3,13 +3,41 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(morgan('short'))
-const connection = mysql.createConnection({
+app.use(express.static('./public'))
+
+function getConnection() {
+ return mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'lbta_mysql'
 })
+}
+
+app.post("/user_create", (req, res) => {
+  
+  console.log("Try to create a new user..." + req.body.create_first_name)
+
+  const firstName = req.body.create_first_name
+  const lastName = req.body.create_last_name
+
+  const query = "INSERT INTO users (first_name, last_name) VALUES (?, ?)"
+
+  getConnection().query(query, [firstName, lastName], (err, results, fields) => {
+    if (err) {
+      res.sendStatus(500)
+      return
+    }
+
+    res.sendStatus(200)
+    console.log("Inserted a new user with id: " + results)
+
+  })
+})
+
 
 app.get("/", (request, response) => {
   console.log("Responding to root route");
@@ -23,10 +51,10 @@ app.listen(3000, () => {
 
 app.get("/user/:id", (req, res) => {
     console.log("Fetching user with id: " + req.params.id)
-
+    
     const userID =  req.params.id
     const query = "SELECT * FROM users WHERE id = ?"
-    connection.query(query, [userID], (err, rows, fields) => {
+    getConnection().query(query, [userID], (err, rows, fields) => {
       if (err) {
         res.sendStatus(500)
         return
@@ -38,7 +66,7 @@ app.get("/user/:id", (req, res) => {
 
 app.get("/users", (req, res) => {
   const query = "SELECT * FROM users"
-    connection.query(query, (err, rows, fields) => {
+    getConnection().query(query, (err, rows, fields) => {
       if (err) {
         res.sendStatus(500)
         return
